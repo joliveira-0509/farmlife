7ced2345b2e63d68b5ffa926593d4c6ee621df24
João <joaovitorsilvadeoliveira82@gmail.com>
2025-02-05 08:24:09 -0400

    farmlifeinit

---
+++ src/Header/index.js
@@ -0,0 +1,50 @@
+"use client"
+import Link from "next/link";
+import style from "./header.module.css";
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className={style.header}>
+
+            <section className={style.navegador}>
+                <Image
+                    className={style.imagem}
+                    src={"./imagens/farm life (1).svg"}
+                    alt={"imagem"}
+                    width={100}
+                    height={100}
+                />
+                <div className={style.name}>
+                    <h1>FARM LIFE</h1>
+                    <h2>~ health center ~</h2>
+                </div>
+                <button
+                    id="botao" className={style.botao}
+                >
+                    <h1>Acessar</h1>
+                </button>
+
+            </section>
+
+            <section className={style.acessos}>
+                <nav>
+                    <ul className={style.li}>
+                        <li >
+                            <Link href="/sobre" className={style.link}>Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/state" className={style.link}>Paciente</Link>
+                        </li>
+                        <li>
+                            <Link href="/listas" className={style.link}>Médico</Link>
+                        </li>
+                        <li>
+                            <Link href="/props" className={style.link}>Listar</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+    );
+};
