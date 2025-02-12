d6e1dcf438874b3413f83ff5ca9250c4c5300d5a
João <joaovitorsilvadeoliveira82@gmail.com>
2025-02-12 08:25:48 -0400

    subsmenu

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,8 +2,22 @@
 import Link from "next/link";
 import style from "./header.module.css";
 import Image from "next/image";
+import React, { useState } from 'react';
 
 export default function Header() {
+    const [medicoOpen, setMedicoOpen] = useState(false);
+    const [pacienteOpen, setPacienteOpen] = useState(false);
+    const [consultaOpen, setConsultaOpen] = useState(false);
+
+    const medicoMouseOpen = () => setMedicoOpen(true);
+    const medicoMouseClose = () => setMedicoOpen(false);
+
+    const pacienteMouseOpen = () => setPacienteOpen(true);
+    const pacienteMouseClose = () => setPacienteOpen(false);
+
+    const consultaMouseOpen = () => setConsultaOpen(true);
+    const consultaMouseClose = () => setConsultaOpen(false);
+
     return (
         <header className={style.header}>
 
@@ -30,21 +44,62 @@ export default function Header() {
             <section className={style.acessos}>
                 <nav>
                     <ul className={style.li}>
-                        <li >
+                        <li>
                             <Link href="/" className={style.link}>Home</Link>
                         </li>
-                        <li>
-                            <Link href="/state" className={style.link}>Paciente</Link>
-                        </li>
-                        <li>
-                            <Link href="/listas" className={style.link}>Médico</Link>
-                        </li>
-                        <li>
-                            <Link href="/props" className={style.link}>Listar</Link>
-                        </li>
+                        <div
+                            className={style.medico}
+                            onMouseEnter={medicoMouseOpen}
+                            onMouseLeave={medicoMouseClose}
+                        > medicos
+                            {medicoOpen && (
+                                <ul>
+                                    <li>Listar Registros</li>
+                                    <li> <Link href='/medicos'>Buscar</Link></li>
+                                    <li>Adicionar Novo</li>
+                                    <li>Editar</li>
+                                    <li>Excluir</li>
+                                </ul>
+                            )}
+
+                        </div>
+                        <div
+                            className={style.paciente}
+                            onMouseEnter={pacienteMouseOpen}
+                            onMouseLeave={pacienteMouseClose}
+                        > Pacientes
+                            {pacienteOpen && (
+                                <ul>
+                                    <li>Listar Registros</li>
+                                    <li>Buscar</li>
+                                    <li>Adicionar Novo</li>
+                                    <li>Editar</li>
+                                    <li>Excluir</li>
+                                </ul>
+                            )}
+                        </div>
+                        <div
+                            className={style.agendamento}
+                            onMouseEnter={consultaMouseOpen}
+                            onMouseLeave={consultaMouseClose}
+                        >Agendamentos
+                            {consultaOpen && (
+                                <ul>
+                                    <li>Listar Consultas</li>
+                                    <li>Agendar Consulta</li>
+                                    <li>Reagendar</li>
+                                    <li>Cancelar</li>
+                                </ul>
+                            )}
+                        </div>
+
+
                     </ul>
                 </nav>
             </section>
         </header>
     );
 };
+
+
+// Menu.js
---
+++ src/app/pacientes/page.js
@@ -0,0 +1,6 @@
+'use client'
+
+export default function Home() {
+    return (
+        <h1>TEste de pagina</h1>
+    )}
