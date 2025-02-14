aeb339410c2770963646c2207ea4352f2ba21922
João <joaovitorsilvadeoliveira82@gmail.com>
2025-02-14 07:55:17 -0400

    header.final

--- src/Header/index.js
+++ src/Header/index.js
@@ -92,8 +92,6 @@ export default function Header() {
                                 </ul>
                             )}
                         </div>
-
-
                     </ul>
                 </nav>
             </section>
